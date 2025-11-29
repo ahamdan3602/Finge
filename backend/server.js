027102219c4924b30486c34d09f8824a317effa0
Abdul Al-rahman Mohammed Hamdan <ahamdan4@uwo.ca>
2025-11-29 18:32:51 -0500

    mongodb deployment

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,19 @@
+import dotenv from "dotenv";
 import express from "express";
-// import * as mongoConfig from "./mongoConfig";
+import mongoose from "mongoose";
 import { run } from "./mongoConfig.js";
+import User from "./user.model.js";
+
+dotenv.config();
+const MONGODB_URI = process.env.DATABASE_URL || "";
+
 const app = express();
 const PORT = 3000;
 run();
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => console.log("Successfully connect to mongoDB"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
 // Add basic middlewarex
 app.use(express.json());
@@ -13,7 +23,13 @@ app.get("/", (req, res) => {
   res.send("Server is running!");
 });
 
-app.listen(PORT, (err) => {
+app.listen(PORT, async (err) => {
   if (err) console.log("Error in server setup");
   console.log("Server listening on Port", PORT);
+  const newUser = new User({
+    username: "Alice",
+    email: "alice@example.com",
+    age: 30,
+  });
+  await newUser.save();
 });
